ad3b0db09235157ed4fc106798689ecc68d13da9
Lukas Stanek <lukas@stanek.xyz>
2018-12-05 23:41:01 +0100

    track keypresses and mouse movements for detecting inactivity

--- src/main/kotlin/timepiece/Timepiece.kt
+++ src/main/kotlin/timepiece/Timepiece.kt
@@ -8,31 +8,79 @@ import java.time.temporal.ChronoUnit
 import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.concurrent.schedule
+import kotlin.coroutines.suspendCoroutine
+import kotlinx.coroutines.*
 
+const val ACTIVITY_INACTIVE = "INACTIVE"
 
-val logFile = "./window.log"
-val interval: Long = 5 * 60 * 1000
+const val logFile = "./window.log"
+const val windowTitleInterval: Long = 60 * 1000
+const val timeUntilInactivity = 60 * 5 // in secs
+
+var timestampLastInput: Instant = Instant.now()
 
 fun main(args: Array<String>){
-    if(args.size > 0 && args[0].equals("evaluate")) {
-        evaluateTodaysUsage()
+    if (args.size > 1 && args[0].equals("report")){
+        reportOnTimeTracked()
     }else{
-        println("Start tracking windows")
-        println()
-        periodicallyCheckWindowTitle()
+        trackActivity()
+    }
+
+}
+
+fun trackActivity() {
+    GlobalScope.launch {
+        listenForInputEvents()
+    }
+    periodicallyCheckWindowTitle()
+}
+
+
+fun periodicallyCheckWindowTitle() {
+    Timer().schedule(100, windowTitleInterval) {
+        "xdotool getwindowfocus getwindowname".runCommand()?.trim()?.apply {
+            val file = File(logFile)
+            file.exists()
+            val activeWindowRecord = ActiveWindowRecord(Instant.now(), this)
+
+            var activity = activeWindowRecord.windowTitle
+            if(Duration.between(timestampLastInput,  Instant.now()).toSeconds() > timeUntilInactivity){
+                activity = ACTIVITY_INACTIVE
+            }
+
+            val record = "${activeWindowRecord.date},$activity\n"
+            print(record)
+            file.appendText(record)
+        }
+
     }
 }
 
-fun evaluateTodaysUsage() {
+suspend fun listenForInputEvents() {
+    val process = ProcessBuilder("xinput", "test-xi2", "--root")
+        .redirectOutput(ProcessBuilder.Redirect.PIPE)
+        .start()
+
+    return suspendCoroutine {
+        process.inputStream.bufferedReader().forEachLine {
+            if(it.startsWith("EVENT type 3") || it.startsWith("EVENT type 6")){
+                timestampLastInput = Instant.now()
+                println(it)
+            }
+        }
+    }
+}
+
+fun reportOnTimeTracked() {
     val now = Instant.now().truncatedTo(ChronoUnit.DAYS)
-    val list =
+
     File(logFile).readLines().map {
         val split = it.split(",")
         ActiveWindowRecord(Instant.parse(split[0]), split[1])
     }.filter {
         now == it.date.truncatedTo(ChronoUnit.DAYS)
     }.fold(mutableListOf<ActiveWindowPeriod>()){
-        list, it ->
+            list, it ->
         val lastOrNull = list.lastOrNull()
 
         if(lastOrNull != null && lastOrNull.windowTitle.equals(it.windowTitle)){
@@ -44,24 +92,9 @@ fun evaluateTodaysUsage() {
         list
     }.also {
         println(it.joinToString("\n"))
-
     }
 
 }
-
-fun periodicallyCheckWindowTitle() {
-    Timer().schedule(100, interval) {
-        "xdotool getwindowfocus getwindowname".runCommand()?.trim()?.apply {
-            val file = File(logFile)
-            file.exists()
-            val activeWindowRecord = ActiveWindowRecord(Instant.now(), this)
-            println(activeWindowRecord)
-            file.appendText("${activeWindowRecord.date},${activeWindowRecord.windowTitle}\n")
-        }
-
-    }
-}
-
 
 fun String.runCommand(): String? {
     try {
